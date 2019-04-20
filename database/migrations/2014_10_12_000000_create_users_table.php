21d563f015cd3acb036f73b862608914cb825a2a
Kevin <yonathan.kevin20@gmail.com>
2019-04-20 22:48:50 +0700

    auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username', 63);
+            $table->string('name', 63);
+            $table->string('email', 63)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
